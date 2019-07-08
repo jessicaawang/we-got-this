1080b80d19a20e7c0757f77676fcc7aa75926b65
Amber Ross <ross.amber1540@gmail.com>
2019-07-08 14:54:27 -0700

    First Cool Save

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+var str = "Hello, playground"
+var age = 16
+"I am \(age) years old"
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1) \(string2)"
+var anotherWelcome = string1 + " " + string2
+print ("Hello, world")
+var first = "Karlie"
+var last = "Kloss"
+print (first)
+print ("\(first)\(last)")
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+print ((a+b+c+d) / 4)
+var double = 10.25
+var integer = 20
+Double(integer)*double
+var celeb = "BILLIE EILISH"
+print ("Happy birthday to \(celeb)")
+var StringtoCompress = "Jessica"
+print ("\(StringtoCompress.first!)\(StringtoCompress.count-2)\(StringtoCompress.last!)")
+5 < 3
+12 > 7
+6 != 8
+7 == 7
+"karlie" == "karlie"
+"karlie" == " karlie"
+"Karlie" == "karlie"
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+var dogAge = 1
+if dogAge < 2 {
+    print("You are a puppy")
+    
+}
+dogAge == 1
+dogAge == 4
+print ("dogAge == 1")
+if dogAge < 2 {
+    print("You are a puppy")
+    
+}
+else if dogAge > 12 {
+    print("You are elderly")
+    
+}
+else {
+    print("You are also awesome")
+    
+}
+
+var favoriteFood = "poke"
+if favoriteFood == "poke" {
+    print("It tastes good")
+    
+}
+if favoriteFood != "poke" {
+    print("It is good too")
+    
+}
+
+else if favoriteFood == "Starbucks" {
+    print("That is cool too")
+    
+}
+
+var hasAccount = "false"
+if hasAccount == "false" {
+    print("Let's get some information to create an account for you")
+
+}
+
